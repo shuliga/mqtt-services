435e94ca9e80d29ae1cdbea2aee101f99a60ab24
Yuriy Shuliha <yuriy_shuliha@epam.com>
2021-10-20 15:29:21 +0300

    Booking integration added

--- booking_sync.sh
+++ booking_sync.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-MQTT_USER=thermo-01; MQTT_PASS=ThermoPass; MQTT_HOST=mqtt.poweroid.io; MQTT_PORT=12674; python tlintegration/tlintegration.py
+MQTT_USER=thermo-01; MQTT_PASS=ThermoPass; MQTT_HOST=mqtt.poweroid.io; MQTT_PORT=12674; python services/tlintegration/tlintegration.py
